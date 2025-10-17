2244d69636d170351d368e979af0558e91aa1a6d
Ikra Nur Kurtoglu <kurtogluikranur@gmail.com>
2025-10-17 13:52:51 +0300

    The files have been organized

--- types/user.ts
+++
@@ -1,9 +0,0 @@
-import { ImageSourcePropType } from 'react-native';
-
-export interface User {
-    id: string;
-    name: string;
-    surname?: string;
-    email?: string;
-    avatar?: ImageSourcePropType;
-}
